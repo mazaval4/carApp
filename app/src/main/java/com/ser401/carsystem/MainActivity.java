1d3901f47e1769c73e262f4b8dd634df89b6f1e6
Zachary Jaros <Zachary.Jaros@asu.edu>
2016-11-06 15:04:49 -0800

    Revert "Car Portion First Commit"
    This reverts commit 580b7b2997e255aa21e097d382f4499c76b50298.

--- app/src/main/java/com/ser401/carsystem/MainActivity.java
+++ app/src/main/java/com/ser401/carsystem/MainActivity.java
@@ -3,9 +3,6 @@ package com.ser401.carsystem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -13,38 +10,5 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        CarTouchListener listener = new CarTouchListener();
-
-        ImageButton carButton = (ImageButton)findViewById(R.id.carBtn);
-        carButton.setOnTouchListener(listener);
     }
-
-    public class CarTouchListener implements View.OnTouchListener{
-        @Override
-        public boolean onTouch(View v, MotionEvent e){
-            int button = v.getId();
-            switch(button){
-                case(R.id.carBtn):
-                    setContentView(R.layout.car_layout_step2);
-                    ImageButton windowButton = (ImageButton)findViewById(R.id.windowBtn);
-                    windowButton.setOnTouchListener(new CarTouchListener());
-                    ImageButton wiperButton = (ImageButton)findViewById(R.id.wiperBtn);
-                    wiperButton.setOnTouchListener(new CarTouchListener());
-                    break;
-                case(R.id.windowBtn):
-                    setContentView(R.layout.window_layout);
-                    break;
-                case(R.id.wiperBtn):
-                    setContentView(R.layout.wiper_layout);
-                    break;
-            }
-
-            return true;
-        }
-    }
-
-
 }
-
-
